a8771327dcb45c6c8af4a98a09025195a59c77a7
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-02-12 13:28:52 +0100

    Styled heading

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,17 +7,31 @@ interface Props {
 
 export default function App(props: Props) {
     return (
-        <div>
+        <div style={style}>
             <h1>Hello from {props.compiler} and {props.framework}!</h1>
         </div>
     )
 }
 
+const centeredContent: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh"
+}
+
+const theme: React.CSSProperties = {
+    color: "green"
+}
+
+const style = { ...centeredContent, ...theme }
+
 // export class App2 extends React.Component<Props> {
 //     render() {
+//         const {compiler, framework} = this.props //deconstruct
 //         return (
 //             <div>
-//                 <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+//                 <h1>Hello from {compiler} and {framework}!</h1>
 //             </div>
 //         )
 //     }
